497570157f9860fd3c50d48b42855a82d92e9260
waffwaff1 <31948220+waffwaff1@users.noreply.github.com>
2019-03-03 00:56:55 +0530

    f s

---
+++ server.js
@@ -0,0 +1,31 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var hbs = require('express-handlebars');
+const cors = require('cors');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
+var routes = require('./routes/index');
+var index = require('./routes/index');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+
+
+// api routes
+app.use('/api', routes);
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 8080;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
